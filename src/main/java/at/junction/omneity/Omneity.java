2e0b9867cfd4bfbdf8ea0b66823d46666fa11d05
Tom Powell <zifnab@zifnab06.net>
2014-04-11 20:17:56 -0600

    Actually load modules. Yay!

--- src/main/java/at/junction/omneity/Omneity.java
+++ src/main/java/at/junction/omneity/Omneity.java
@@ -1,5 +1,7 @@
 package at.junction.omneity;
 
+import at.junction.omneity.spawn.Spawn;
+import at.junction.omneity.xp.Xp;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandMap;
 import org.bukkit.command.CommandSender;
@@ -7,17 +9,34 @@ import org.bukkit.plugin.SimplePluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Omneity extends JavaPlugin {
 
     Configuration config;
     CommandMap commandMap = null;
+    Map<String, Module> moduleMap = new HashMap<String, Module>();
     @Override
     public void onEnable(){
         config = new Configuration(this);
         config.load();
 
+        //Better way? Not sure.
+        Xp xp = new Xp(this);
+        Spawn spawn = new Spawn(this);
+        for (String module: config.ENABLED_MODULES){
+            if (module.equalsIgnoreCase("xp")){
+                moduleMap.put("xp", xp);
+            } else if (module.equalsIgnoreCase("spawn")){
+                moduleMap.put("spawn", spawn);
+            }
+        }
+
+        for (Module m : moduleMap.values()){
+            m.onEnable();
+        }
         //Do any other setup BEFORE this try/catch
         try {
             Field field = SimplePluginManager.class.getDeclaredField("commandMap");
@@ -35,7 +54,9 @@ public class Omneity extends JavaPlugin {
     }
     @Override
     public void onDisable(){
-
+        for (Module m : moduleMap.values()){
+            m.onDisable();
+        }
     }
 
     @Override
